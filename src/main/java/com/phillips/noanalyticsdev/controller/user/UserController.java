a9d938165c154be9482546a4f8acb25651b9f019
Phillip2Go <phillip.mai@live.de>
2021-07-15 15:37:43 +0200

    fix

--- src/main/java/com/phillips/noanalyticsdev/controller/user/UserController.java
+++ src/main/java/com/phillips/noanalyticsdev/controller/user/UserController.java
@@ -1,11 +1,14 @@
 package com.phillips.noanalyticsdev.controller.user;
 
 import com.phillips.noanalyticsdev.service.user.UserService;
+import com.phillips.noanalyticsdev.util.ProductItem;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin
@@ -18,5 +21,15 @@ public class UserController {
     @GetMapping(value = "/userproducts/{amount}/{userId}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Object getUserProducts(@PathVariable int amount, @PathVariable String userId) {
         return userService.getUserProducts(amount, userId);
+    }
+
+    @GetMapping(value = "/mostusedtag/{userId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public String getMostUsedTag(@PathVariable String userId) {
+        return userService.getMostUsedTag(userId);
+    }
+
+    @GetMapping(value = "/usedtagsdesc/{userId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<String> getUsedTagsDesc(@PathVariable String userId) {
+        return userService.getUsedTagsDesc(userId);
     }
 }
